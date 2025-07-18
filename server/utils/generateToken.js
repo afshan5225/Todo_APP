3e1d874edc034e5bce8ffae3f1e60b3432d89ab0
Muhammed Afshan Anas <114726854+afshan5225@users.noreply.github.com>
2025-07-18 08:22:39 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,9 +7,9 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: false,      
-    sameSite: 'lax',    
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: true,                
+    sameSite: 'None',            
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
