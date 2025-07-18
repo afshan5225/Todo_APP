d1fd9cd2a0c37c7a6cb98594c96168ea93339ead
Muhammed Afshan Anas <114726854+afshan5225@users.noreply.github.com>
2025-07-18 08:08:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,8 +14,7 @@ const app = express();
 const PORT = process.env.PORT ||5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  'https://todo-app-smoky-eight-88.vercel.app'
+  origin: ['http://localhost:5173', 'https://todo-app-smoky-eight-88.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
